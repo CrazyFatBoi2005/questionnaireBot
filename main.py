afc6e824b177364b149ec0842fe550baf0c2100c
crazysadboy <spa.ez@mail.ru>
2022-09-04 20:48:15 +0300

    v0.952

--- main.py
+++ main.py
@@ -471,6 +471,23 @@ async def build_media(bot, id):
                 media.attach_document(file)
                 await bot.send_media_group(ADMIN, media=media)
                 await bot.send_media_group(ADMIN2, media=media)
+    for p in data[id]['peoples']:
+        await asyncio.sleep(1)
+        media = types.MediaGroup()
+        file = types.InputFile(p.passport)
+        filename, file_extension = os.path.splitext(p.passport)
+        if file_extension == '.pdf':
+            media.attach_document(file)
+            await bot.send_media_group(ADMIN2, media=media)
+        else:
+            try:
+                media = types.MediaGroup()
+                media.attach_photo(file, p.name)
+                await bot.send_media_group(ADMIN2, media=media)
+            except:
+                media = types.MediaGroup()
+                media.attach_document(file)
+                await bot.send_media_group(ADMIN2, media=media)
 
 
 def build_message(id):
